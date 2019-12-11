235d6eccd853d3e89daa7516de0f9a9738b88a7d
Fullcreammilk <852805936@qq.com>
2019-12-11 21:07:53 +0800

    ncl

--- test3.py
+++ test3.py
@@ -2,12 +2,23 @@ import os
 import tkinter as tk
 from tkinter import *
 from PIL import Image,ImageTk
+from tkinter import ttk
+
+
+d_area={"北部zq":"BBZQ", "南部zq":"NBZQ", "中部zq":"ZBZQ", "西部zq":"XBZQ", "兰州区域":"LZRCC"
+                ,"乌鲁木齐区域":"WLMQRCC", "沈阳区域":"SYRCC", "北京区域":"BJRCC", "上海区域":"SHRCC", "广州区域":"GZRCC"
+                , "成都区域":"CDRCC", "武汉区域":"WHRCC" , "北半球":"EQ_NH", "一带一路":"Road_Belt","东亚":"EA"
+                ,"南亚":"SA","东亚-南亚":"SA_EA","西北太平洋-北印度洋":"NWP_NIO"}
+
+d_factor={"160站降水距平百分率":"rain160x", "160站气温异常":"temp160", "台风GPI":"gpi", "降水异常":"precip", "降水距平百分率":"precipx", "气温异常":"air"}
+
+d_season={"春":"MAM", "夏":"JJA", "秋":"SON", "冬":"DJF"}
 
 
 class RootWin():
     def __init__(self):
         self.root = tk.Tk()
-        self.root.geometry("800x400")  # 窗口大小
+        self.root.geometry("800x450")  # 窗口大小
 
         self.first=StringVar()
         self.second=StringVar()
@@ -39,33 +50,41 @@ class RootWin():
         mainframe.columnconfigure(0, weight=1)
         mainframe.rowconfigure(0, weight=1)
 
-        year = StringVar()
-        season = StringVar()
-        area = StringVar()
-        factor = StringVar()
-        precision = StringVar()
-
         mainframe.label_year = Label(mainframe, text="年份:")
         mainframe.label_season = Label(mainframe, text="季节:")
         mainframe.label_area = Label(mainframe, text="地区:")
         mainframe.label_factor = Label(mainframe, text="要素:")
         mainframe.label_precision = Label(mainframe, text="检验:")
 
-        mainframe.entry_year = Entry(mainframe, relief=SUNKEN, textvariable=year)
-        mainframe.entry_season = Entry(mainframe, relief=SUNKEN, textvariable=season)
-        mainframe.entry_area = Entry(mainframe, relief=SUNKEN, textvariable=area)
-        mainframe.entry_factor = Entry(mainframe, relief=SUNKEN, textvariable=factor)
-        mainframe.entry_precision = Entry(mainframe, relief=SUNKEN, textvariable=precision)
+        mainframe.entry_year = ttk.Combobox(mainframe)
+        mainframe.entry_year["value"] = ("2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019")
+        mainframe.entry_year.current(0)
+        mainframe.entry_season = ttk.Combobox(mainframe)
+        mainframe.entry_season["value"] = ("春", "夏", "秋", "冬")
+        mainframe.entry_season.current(0)
+        mainframe.entry_area = ttk.Combobox(mainframe)
+        mainframe.entry_area["value"] = ("北部zq", "南部zq", "中部zq", "西部zq", "兰州区域",
+                                         "乌鲁木齐区域", "沈阳区域", "北京区域", "上海区域", "广州区域"
+                                         , "成都区域", "武汉区域", "北半球", "一带一路","东亚","南亚","东亚-南亚","西北太平洋-北印度洋")
+        mainframe.entry_area.current(0)
+        mainframe.entry_factor = ttk.Combobox(mainframe)
+        mainframe.entry_factor["value"] = ("160站降水距平百分率", "160站气温异常", "台风GPI", "降水异常", "降水距平百分率", "气温异常")
+        mainframe.entry_factor.current(0)
+        mainframe.entry_precision = ttk.Combobox(mainframe)
+        mainframe.entry_precision["value"] = ("ACC", "PS")
 
-        mainframe.button_ok = Button(mainframe, text="确认", width=20,
-                                     command=lambda a=year, b=season, c=area, d=factor, e=precision: {self.execute(a,b,c,d,e),
-                                                                                                      top.destroy()})
-        mainframe.button_cancel = Button(mainframe, text="清除", width=20,
-                                         command=lambda a=year, b=season, c=area, d=factor, e=precision: {a.set(''),
-                                                                                                          b.set(''),
-                                                                                                          c.set(''),
-                                                                                                          d.set(''),
-                                                                                                          e.set('')})
+
+        mainframe.button_ok = Button(mainframe, text="确认", width=22,
+                                     command=lambda : { self.execute(mainframe.entry_year.get(),d_season[mainframe.entry_season.get()]
+                                                        ,d_area[mainframe.entry_area.get()],d_factor[mainframe.entry_factor.get()],
+                                                        mainframe.entry_precision.get()),
+                                                        top.destroy()})
+        mainframe.button_cancel = Button(mainframe, text="清除", width=22,
+                                         command=lambda : { mainframe.entry_year.current(0),
+                                                            mainframe.entry_season.current(0),
+                                                            mainframe.entry_area.current(0),
+                                                            mainframe.entry_factor.current(0),
+                                                            mainframe.entry_precision.current(0)})
 
         mainframe.label_year.grid(row=0, column=0, sticky=(N, W))
         mainframe.label_season.grid(row=1, column=0, sticky=(N, W))
@@ -80,6 +99,7 @@ class RootWin():
         mainframe.entry_factor.grid(row=3, column=1, sticky=(N, W))
         if wintype == 'SPS预测回报':
             mainframe.entry_precision.grid(row=4, column=1, sticky=(N, W))
+            mainframe.entry_precision.current(0)
 
         mainframe.button_ok.grid(row=5, column=0, sticky=(N, W))
         mainframe.button_cancel.grid(row=5, column=1, sticky=(N, W))
@@ -90,13 +110,16 @@ class RootWin():
         url1="resource"
         url2="resource"
         for dir in dirs:
-            if year.get() =='' or season.get() =='' or area.get() =='' or factor.get() =='':
-                url1="resource/error.png"
-                url2 = "resource/error.png"
-            elif year.get() in dir and season.get() in dir and area.get() in dir and factor.get() in dir:
+            if year in dir and season in dir and area in dir and factor in dir:
                 url1=url1+'/'+dir
-                if precision.get() != '':
+                break
+        if precision == '':
+            url2="resource/error.png"
+        else:
+            for dir in dirs:
+                if year in dir and season in dir and area in dir and factor in dir and 'forecast' not in dir:
                     url2=url2+'/'+dir
+                    break
 
         if len(url1)<=8:
             url1="resource/error.png"
@@ -115,6 +138,9 @@ class RootWin():
 
         label_img1.grid(row=0, column=0, sticky=W)
         label_img2.grid(row=0, column=1, sticky=E)
+
+        if precision != '' and url2 != 'resource/error.png':
+            Label(self.root,text='PS=74.00 ACC=0.13', compound=CENTER).grid(row=1,column=1)
 
 
 rootWin=RootWin()
